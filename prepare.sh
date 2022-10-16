9f3aa302c4e59eca925a24b528dc590de26d3454
DevAdalat <akbusiness022@gmail.com>
2022-10-16 19:42:53 +0530

    Some fix

--- prepare.sh
+++ prepare.sh
@@ -6,21 +6,12 @@ if [ ! -d "td" ]; then
 	cd $__DIR__
 	git clone https://github.com/tdlib/td.git
 	cd td
-	# How to get the hash if there is no version tag:
-	# - Goto https://github.com/tdlib/td/blame/master/CMakeLists.txt
-	# - Check the line of `project(TDLib VERSION...
-	# - Click left commit link
-	# - Copy and paste the commit hash, e.q. git checkout <hash>
-	# git checkout tags/v1.8.1
-	git checkout 92c2a9c4e521df720abeaa9872e1c2b797d5c93f
 	cd ..
 fi
 
 if [ ! -d "openssl" ]; then
 	cd $__DIR__
-	git clone https://github.com/PurpleI2P/OpenSSL-for-Android-Prebuilt.git
-	mv OpenSSL-for-Android-Prebuilt/openssl-1.1.1k-clang openssl
-	rm -fr OpenSSL-for-Android-Prebuilt
+	./build-openssl.sh
 fi
 
 # Need to generate some files first, see https://github.com/tdlib/td/issues/1077#issuecomment-640056388
