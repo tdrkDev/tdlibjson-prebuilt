36319c03c21803b47e44ed23d829975f4dc4bb8e
DevAdalat <akbusiness022@gmail.com>
2022-10-17 10:00:49 +0530

    Testing

--- prepare.sh
+++ prepare.sh
@@ -1,17 +1,25 @@
 #!/bin/bash -e
-echo y | sudo apt install gperf
+
 __DIR__="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 
 if [ ! -d "td" ]; then
 	cd $__DIR__
 	git clone https://github.com/tdlib/td.git
 	cd td
+	# How to get the hash if there is no version tag:
+	# - Goto https://github.com/tdlib/td/blame/master/CMakeLists.txt
+	# - Check the line of `project(TDLib VERSION...
+	# - Click left commit link
+	# - Copy and paste the commit hash, e.q. git checkout <hash>
+	# git checkout tags/v1.8.1
 	cd ..
 fi
 
 if [ ! -d "openssl" ]; then
 	cd $__DIR__
-	tar -xf pre-openssl.tar.gz
+	git clone https://github.com/PurpleI2P/OpenSSL-for-Android-Prebuilt.git
+	mv OpenSSL-for-Android-Prebuilt/openssl-1.1.1k-clang openssl
+	rm -fr OpenSSL-for-Android-Prebuilt
 fi
 
 # Need to generate some files first, see https://github.com/tdlib/td/issues/1077#issuecomment-640056388
